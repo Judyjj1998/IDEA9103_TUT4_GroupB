8c5887ad11e6428630e85116d0e38aae70fbbd05
“ezha8129” <ezha8129@uni.sydney.edu.au>
2023-10-15 22:10:35 +1100

    Made static rectangles
    Co-authored-by: Samantha Lagman <splagz@users.noreply.github.com>
    Co-authored-by: Judyjj1998 <Judyjj1998@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let numberShapes = 50; 
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  noLoop(); //Stopping the draw function so the rectangles are static - reference p5.js website
+
 }
 
 function draw() {
-  background(220);
+  background(255); 
+  //Need to fix so the rectangles dont move 
+  drawShapes();
+    // drawRect();
 }
+
+function drawShapes() {
+  for (let i = 0; i < numberShapes; i++) {
+    fill(255, 255, 255);
+    let x = random(width);
+    let y = random(height);
+    let rectSize = random(10, 50); 
+    rect(x, y, rectSize, rectSize);
+  }
+}
+
+// function drawRect(){
+//   rect(windowWidth/4, windowHeight/3, 50,30,0,0,237);
+//   rect(windowWidth/2, windowHeight/6, 50,30,0,0,237);
+// }
