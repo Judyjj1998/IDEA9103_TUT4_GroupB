56c51ae31fd94b7f865cbe6381784e635c47603d
“ezha8129” <ezha8129@uni.sydney.edu.au>
2023-10-15 22:34:30 +1100

    Made yellow, blue and grey rectangles

--- sketch.js
+++ sketch.js
@@ -3,22 +3,45 @@ let numberShapes = 50;
 function setup() {
   createCanvas(windowWidth, windowHeight); //Creating the canvas to the size of the window
   noLoop(); //Stopping the draw function so the rectangles are static - reference p5.js website
-
+  noStroke();
 }
 
 function draw() {
   background(255); //Setting the background to white
+  
   //Calling the function drawShapes to draw multiple rectangles 
-  drawShapes();
+  drawBlueShapes();
+  drawYellowShapes();
+  drawGreyShapes();
     
 }
 
-function drawShapes() {
+function drawBlueShapes() {
   for (let i = 0; i < numberShapes; i++) {
-    fill(255, 255, 255);
+    fill(0, 0, 240);
     let x = random(width);
     let y = random(height);
-    let rectSize = random(10, 50); 
+    let rectSize = random(100, 200); 
+    rect(x, y, rectSize, rectSize);
+  }
+}
+
+function drawYellowShapes() {
+  for (let i = 0; i < numberShapes; i++) {
+    fill(237, 222, 0);
+    let x = random(width);
+    let y = random(height);
+    let rectSize = random(20, 50); 
+    rect(x, y, rectSize, rectSize);
+    
+  }
+}
+function drawGreyShapes() {
+  for (let i = 0; i < 100; i++) {
+    fill(210);
+    let x = random(width);
+    let y = random(height);
+    let rectSize = random(10, 15); 
     rect(x, y, rectSize, rectSize);
   }
 }
